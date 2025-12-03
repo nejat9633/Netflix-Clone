d798b7f892adaa9020cc0bcbba8ec5db37fe15c9
NJ <nej9633@gmail.com>
2025-12-03 14:34:31 +0300

    deployment

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Banner from './components/Banner/Banner'
